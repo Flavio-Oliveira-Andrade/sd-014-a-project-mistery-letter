420965c13ba86793b48f0bcff8ec9789e00c8c4a
caiquequaresmasilva <caique.quaresma@gmail.com>
2021-07-10 13:25:57 -0300

    Requisito 16

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
 const criarCarta = document.getElementById('criar-carta');
 const cartaTexto = document.getElementById('carta-texto');
 const cartaGerada = document.getElementById('carta-gerada');
-// const estilo = []
+const listOfClasses = [
+  {
+    0: 'newspaper',
+    1: 'magazine1',
+    2: 'magazine2',
+  },
+  {
+    0: 'medium',
+    1: 'big',
+    2: 'reallybig',
+  },
+  {
+    0: 'rotateleft',
+    1: 'rotateright',
+  },
+  {
+    0: 'skewleft',
+    1: 'skewright',
+  }];
+
+const getClasses = () => {
+  const classlist = [];
+  listOfClasses.forEach((style) => {
+    const num = Math.trunc(Math.random() * style.length);
+    classlist.push(style[num]);
+  });
+  return classlist;
+};
 
 criarCarta.addEventListener('click', () => {
   let letter = cartaTexto.value.trim();
@@ -13,6 +40,7 @@ criarCarta.addEventListener('click', () => {
     letter.forEach((word) => {
       const span = document.createElement('span');
       span.innerText = `${word} `;
+      span.classList = getClasses();
       cartaGerada.appendChild(span);
     });
   }
