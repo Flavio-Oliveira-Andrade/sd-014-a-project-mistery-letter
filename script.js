b642fa1c4aa8b69a403cd84cf3b59bf3bcedf540
WilsonSalles-Jnr <wilsonsallesjunior@gmail.com>
2021-07-10 15:30:37 -0300

    faltando 4 para concluir

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const input = document.getElementById('carta-texto')
 const par = document.getElementById('carta-gerada')
 const btn = document.getElementById('criar-carta')
+let estilo = [`newspaper`, `magazine1`, `magazine2`];
+let sizegp = [`medium`, `big`, `reallybig`]
+let rotgp = [`rotateleft`, `rotateright`]
+let skewgp = [`skewleft`, `skewright`]
+
+// let rdmA = estilo[Math.floor(Math.random()*3)]
+// let rdmB = sizegp[Math.floor(Math.random()*3)]
+// let rdmC = rotgp[Math.floor(Math.random()*2)]
+// let rdmD = skewgp[Math.floor(Math.random()*2)]
 
 function gerar(){
     if (input.value === ''){
@@ -15,6 +24,10 @@ function gerar(){
                 string += ' '
                 let span = document.createElement('span');
                 span.innerText = string;
+                span.className += estilo[Math.floor(Math.random()*3)] + " "
+                span.className += sizegp[Math.floor(Math.random()*3)] + " "
+                span.className += rotgp[Math.floor(Math.random()*2)] + " "
+                span.className += skewgp[Math.floor(Math.random()*2)]
                 string = ''
                 par.appendChild(span)
                 // par.innerText += ' '
@@ -22,6 +35,10 @@ function gerar(){
     
         }
         let lastSpan = document.createElement('span')
+        lastSpan.className += estilo[Math.floor(Math.random()*3)] + " "
+        lastSpan.className += sizegp[Math.floor(Math.random()*3)] + " "
+        lastSpan.className += rotgp[Math.floor(Math.random()*2)] + " "
+        lastSpan.className += skewgp[Math.floor(Math.random()*2)]
         lastSpan.innerText = string;
         par.appendChild(lastSpan)
 
