fa72164440985493d2fba169e0017a4c26829e09
Gustavo Dolzan <gudolzan@gmail.com>
2021-07-11 20:25:17 -0300

    Personalizes styles and fixes bugs

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 const input = document.getElementById('carta-texto');
 const inputButton = document.getElementById('criar-carta');
 const outputParagraph = document.getElementById('carta-gerada');
-const classes = { // An empty string for each class for none class
-  style: ['', 'newspaper', 'magazine1', 'magazine2'],
-  size: ['', 'medium', 'big', 'reallybig'],
-  rotate: ['', 'rotateleft', 'rotateright'],
-  inclination: ['', 'skewleft', 'skewright'],
-};
+const wordCounter = document.getElementById('carta-contador');
+const classes = [ // An empty string for each class for none class
+  ['newspaper', 'magazine1', 'magazine2'],
+  ['medium', 'big', 'reallybig'],
+  ['rotateleft', 'rotateright'],
+  ['skewleft', 'skewright'],
+];
 
 function randomClasses() {
   let classesRandomized = '';
   for (const group in classes) {
-    classesRandomized += classes[group][Math.floor(Math.random() * (classes[group].length))] + ' ';
+    classesRandomized += `${classes[group][Math.floor(Math.random() * (classes[group].length))]} `;
   }
   return classesRandomized;
 }
@@ -34,7 +35,7 @@ function generateLetter() {
     } else {
       outputParagraph.innerHTML = ''; // Clears past outputs
       const outputArray = input.value.trim().split(' '); // Turns input into array
-      document.getElementById('carta-contador').innerText = `Contagem de palavras: ${outputArray.length}`;
+      wordCounter.innerText = outputArray.length;
       printsLetter(outputArray);
     }
   });
