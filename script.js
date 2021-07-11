3f7a2d05087d2c8b358e56be07f8a314895bdab5
Gustavo Dolzan <gudolzan@gmail.com>
2021-07-11 18:37:15 -0300

    Creates function that sets classes randomized and word counter

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 const input = document.getElementById('carta-texto');
 const inputButton = document.getElementById('criar-carta');
 const outputParagraph = document.getElementById('carta-gerada');
+const classes = { // An empty string for each class for none class
+  style: ['', 'newspaper', 'magazine1', 'magazine2'],
+  size: ['', 'medium', 'big', 'reallybig'],
+  rotate: ['', 'rotateleft', 'rotateright'],
+  inclination: ['', 'skewleft', 'skewright'],
+};
+
+function randomClasses() {
+  let classesRandomized = '';
+  for (const group in classes) {
+    classesRandomized += classes[group][Math.floor(Math.random() * (classes[group].length))] + ' ';
+  }
+  return classesRandomized;
+}
+
+function printsLetter(outputArray) {
+  for (let index = 0; index < outputArray.length; index += 1) {
+    const outputLetter = document.createElement('span');
+    outputLetter.innerText = outputArray[index];
+    while (outputLetter.classList.length < 2) { // Condition for at least two classes
+      outputLetter.className = randomClasses(); // Gets string output
+    }
+    outputParagraph.appendChild(outputLetter);
+  }
+}
 
 function generateLetter() {
   inputButton.addEventListener('click', () => {
@@ -8,12 +33,9 @@ function generateLetter() {
       outputParagraph.innerText = 'Por favor, digite o conteúdo da carta.';
     } else {
       outputParagraph.innerHTML = ''; // Clears past outputs
-      const outputArray = input.value.split(' '); // Turns input into array
-      for (let index = 0; index < outputArray.length; index += 1) {
-        const outputLetter = document.createElement('span');
-        outputLetter.innerText = outputArray[index];
-        outputParagraph.appendChild(outputLetter);
-      }
+      const outputArray = input.value.trim().split(' '); // Turns input into array
+      document.getElementById('carta-contador').innerText = outputArray.length;
+      printsLetter(outputArray);
     }
   });
 }
