8d3fbd4c1df6868491eab68d663bfff51010221d
Williamns <williamnstolb@gmail.com>
2021-07-11 16:00:01 -0300

    Completo: item 5

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 const buttonCriaCarta = document.querySelector('#criar-carta');
 buttonCriaCarta.addEventListener('click', () => {
-  const carta = document.querySelector('#carta-texto').value;
+  let carta = document.querySelector('#carta-texto').value;
   const paragrafo = document.querySelector('#carta-gerada');
+  // Zera paragrafo para nao ter acumulo de span:
   paragrafo.innerHTML = '';
   const array = carta.split(' ');
-  console.log(carta);
-  console.log(array);
-  for (let index = 0; index < array.length; index += 1) {
-    const palavra = document.createElement('span');
-    palavra.innerText = array[index];
-    paragrafo.appendChild(palavra);
+  // Verfica a condicao de o usuário digitar algo diferete de espaço vazio:  
+  if (carta !== '' && array[0] !== '') {
+    // Cria a tag span para cada palavra:
+    for (let index = 0; index < array.length; index += 1) {
+      const palavra = document.createElement('span');
+      palavra.innerText = array[index];
+      paragrafo.appendChild(palavra);
+    }
+  } else {
+    paragrafo.innerText = 'Por favor, digite o conteúdo da carta.';
   }
 });
