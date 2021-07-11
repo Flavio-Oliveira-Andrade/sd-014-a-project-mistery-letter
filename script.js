8fa44b6194d33ca1c104e06b8ae3bf16038cf811
Heitor SF <heitor.sf5@gmail.com>
2021-07-10 23:15:17 -0300

    5 done. 4 was done as a collateral before.

--- script.js
+++ script.js
@@ -2,14 +2,20 @@ const letterDisplay = document.querySelector('#carta-gerada');
 const writeDown = document.querySelector('#carta-texto');
 const inputBtn = document.querySelector('#criar-carta');
 
-inputBtn.addEventListener('click', AddRansom);
-
+// each word in input is put into the letter in individual span tags
 function AddRansom() {
   letterDisplay.innerHTML = '';
+  if (writeDown.value === '' || writeDown.value === ' ') {
+    letterDisplay.innerHTML = 'Por favor, digite o conteúdo da carta.';
+    return;
+  }
   let rnsmWords = writeDown.value.split(' ');
-    for (let i = 0; i < rnsmWords.length; i++) {
-    let rnsmBlock = document.createElement('span');
+  for (let i = 0; i < rnsmWords.length; i+=1) {
+    const rnsmBlock = document.createElement('span');
     rnsmBlock.innerText = rnsmWords[i];
     letterDisplay.appendChild(rnsmBlock);
   }
-};
+}
+
+// add event listener corner
+inputBtn.addEventListener('click', AddRansom);
