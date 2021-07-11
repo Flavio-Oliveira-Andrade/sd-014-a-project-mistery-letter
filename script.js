613d817c08828a5a133aaa17ab0f49cb05c844f1
Bruno Feu <brunofeu89@gmail.com>
2021-07-11 20:29:30 -0300

    requisitos 4 5 6 7 8 9

--- script.js
+++ script.js
@@ -2,17 +2,20 @@ const btnCreate = document.getElementById('criar-carta');
 const cartaGerada = document.getElementById('carta-gerada');
 const textInput = document.getElementById('carta-texto');
 
-
-
-
-
-
 function createLetter() {
-  const letter = document.createElement('span');
-  const word = textInput.value;
-  letter.innerHTML = word;
-  cartaGerada.appendChild(letter);
-
+  const words = textInput.value.split(' ');
+  while (cartaGerada.firstChild) {
+    cartaGerada.removeChild(cartaGerada.lastChild);
+  }
+  if (words === [] || textInput.value.trim().length === 0) { // https://stackoverflow.com/questions/10261986/how-to-detect-string-which-contains-only-spaces
+    cartaGerada.innerHTML = 'Por favor, digite o conteúdo da carta.';
+  } else {
+    for (let i = 0; i < words.length; i += 1) {
+      const letter = document.createElement('span');
+      letter.innerHTML = words[i];
+      cartaGerada.appendChild(letter);
+    }
+  }
 }
 
 btnCreate.addEventListener('click', createLetter);
