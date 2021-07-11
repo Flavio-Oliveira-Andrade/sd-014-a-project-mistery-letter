4b59dc20f6ff7421eaff5cc7621eabd491e15597
Gustavo Dolzan <gudolzan@gmail.com>
2021-07-11 16:50:02 -0300

    Corrects warning/output when no input is entered

--- script.js
+++ script.js
@@ -5,15 +5,12 @@ const outputParagraph = document.getElementById('carta-gerada');
 function generateLetter() {
   inputButton.addEventListener('click', () => {
     if (input.value.trim() === '') { // .trim() removes beginning and endind whitespaces
-      alert('Por favor, digite o conteúdo da carta.');
+      outputParagraph.innerText = 'Por favor, digite o conteúdo da carta.';
     } else {
       const outputArray = input.value.split(' '); // Turns input into array
-
       for (let index = 0; index < outputArray.length; index += 1) {
         const outputLetter = document.createElement('span');
-
         outputLetter.innerText = outputArray[index];
-
         outputParagraph.appendChild(outputLetter);
       }
     }
