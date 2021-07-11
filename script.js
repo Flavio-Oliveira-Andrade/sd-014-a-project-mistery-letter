147167c42bebf32396ded7fe9224a576b4c85c80
Ilan Aragão <ilan_aragao@hotmail.com>
2021-07-10 21:22:40 -0300

    Finaliza requisito 5

--- script.js
+++ script.js
@@ -4,11 +4,15 @@ function gerarCarta() {
   const input = document.getElementById('carta-texto').value;
   const card = document.getElementById('carta-gerada');
   card.innerHTML = '';
-  const cartas = input.split(' ');
-  for (let index = 0; index < cartas.length; index += 1) {
-    const element = document.createElement('span');
-    element.innerHTML = cartas[index];
-    card.appendChild(element);
+  if (input.trim() !== '') {
+    const cartas = input.split(' ');
+    for (let index = 0; index < cartas.length; index += 1) {
+      const element = document.createElement('span');
+      element.innerHTML = cartas[index];
+      card.appendChild(element);
+    }
+  } else {
+    card.innerText = 'Por favor, digite o conteúdo da carta.';
   }
 }
 
