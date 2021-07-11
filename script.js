20a9533c9368cf8e550a43d38fed6e53ef141174
Bruno Feu <brunofeu89@gmail.com>
2021-07-11 20:49:40 -0300

    adiciona classes e funcao para randomizar

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ const btnCreate = document.getElementById('criar-carta');
 const cartaGerada = document.getElementById('carta-gerada');
 const textInput = document.getElementById('carta-texto');
 
+const estilo = ['newspaper', 'magazine1', 'magazine2'];
+const tamanho = ['medium', 'big', 'reallybig'];
+const rotacao = ['rotateleft', 'rotateright'];
+const inclinacao = ['skewleft', 'skewright'];
+
+function rdn(array) {
+  return array[Math.floor(Math.random() * array.length)];
+  // https://stackoverflow.com/questions/9286473/whats-the-equivalent-of-sample-in-javascript
+}
+
 function createLetter() {
   const words = textInput.value.split(' ');
   while (cartaGerada.firstChild) {
@@ -13,6 +23,7 @@ function createLetter() {
     for (let i = 0; i < words.length; i += 1) {
       const letter = document.createElement('span');
       letter.innerHTML = words[i];
+      letter.className = [rdn(estilo), rdn(tamanho), rdn(rotacao), rdn(inclinacao)].join(' ');
       cartaGerada.appendChild(letter);
     }
   }
