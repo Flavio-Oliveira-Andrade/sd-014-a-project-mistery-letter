15288f96acd256fbc436f36d3a0537f36a212058
Paulo Renan Almeida <able.paulorenan@gmail.com>
2021-07-12 15:58:31 -0300

    Requisito 5

--- script.js
+++ script.js
@@ -4,18 +4,22 @@ const carta = document.getElementById('carta-gerada');
 const cadaPal = document.getElementsByClassName('palavras-separadas');
 
 function gerarCarta() {
-  const arrayTexto = myInput.value.split(' ');
-  for (let i = 0; i < cadaPal.length; i += 1){
-    const element = cadaPal[i];
-    carta.removeChild(element);
-    i -= 1;
-  }
-  for (let i = 0; i < arrayTexto.length; i += 1) {
-    const palInp = document.createElement('span');
-    palInp.innerText = arrayTexto[i];
-    palInp.classList.add('palavras-separadas');
-    carta.appendChild(palInp);
+  carta.innerText = '';
+  if (myInput.value.trim() === '') {
+    carta.innerText = 'Por favor, digite o conteúdo da carta.'
+  } else {
+    const arrayTexto = myInput.value.split(' ');
+    for (let i = 0; i < cadaPal.length; i += 1) {
+      const element = cadaPal[i];
+      carta.removeChild(element);
+      i -= 1;
+    }
+    for (let i = 0; i < arrayTexto.length; i += 1) {
+      const palInp = document.createElement('span');
+      palInp.innerText = arrayTexto[i];
+      palInp.classList.add('palavras-separadas');
+      carta.appendChild(palInp);
+    }
   }
 }
-
 inputButton.addEventListener('click', gerarCarta);
