b554dbedb75b58610985eb0267228d009431d67a
Jonathan Ferreira <j0n4t@hotmail.com>
2021-07-11 23:04:45 -0300

    Requisito 5

--- script.js
+++ script.js
@@ -3,13 +3,17 @@ const writeButton = document.getElementById('criar-carta');
 const misteryLetter = document.getElementById('carta-gerada');
 
 function writeLetter() {
-  misteryLetter.innerHTML = '';
-  const words = inputText.value.split(' ');
-  words.forEach((word) => {
-    const span = document.createElement('span');
-    span.innerText = word;
-    misteryLetter.append(span);
-  });
+  if (inputText.value.trim() === '') {
+    misteryLetter.innerText = 'Por favor, digite o conteúdo da carta.';
+  } else {
+    misteryLetter.innerHTML = '';
+    const words = inputText.value.split(' ');
+    words.forEach((word) => {
+      const span = document.createElement('span');
+      span.innerText = word;
+      misteryLetter.append(span);
+    });
+  }
 }
 
 writeButton.addEventListener('click', writeLetter);
