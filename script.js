023bad6846a85c247be8ed79187650bed23e1c7b
Vincenzo Fedzuirek Di Giacomo <79121466+Vincenzofdg@users.noreply.github.com>
2021-07-12 18:25:04 -0300

    6-15° REQUISITO

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ const textoDigitado = document.getElementById('carta-texto');
 const btnGerarCarta = document.getElementById('criar-carta');
 const corpoDaCarta = document.getElementById('carta-gerada');
 
+
 // GERANDO A CARTA A PARTIR DO INPUT:
 btnGerarCarta.addEventListener('click', function () {
     while (corpoDaCarta.firstChild) {
         corpoDaCarta.removeChild(corpoDaCarta.firstChild)
     }
-    let texto = textoDigitado.value.split(' ');
-
     if (textoDigitado.value.replace(/ /g, '') === '') {
         corpoDaCarta.innerText = 'Por favor, digite o conteúdo da carta.';
     } else {
+        let texto = textoDigitado.value.split(' ');
         for (let index = 0; index < texto.length; index++) {
             let gerandoPalavras = document.createElement('span');
             gerandoPalavras.id = 'span';
