5a7da946df5e751a8b7a45a02db32b6b00d41570
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-07-12 17:29:37 -0300

    Add requirement 5

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ const letterContainer = document.getElementById('carta-gerada');
 function createLetter() {
   const button = document.getElementById('criar-carta');
   button.addEventListener('click', () => {
-    const text = document.getElementById('carta-texto').value
+    const text = document.getElementById('carta-texto').value;
     const words = text.split(' ');
+    if (!text || /^\s+$/.test(text)) words[0] = 'Por favor, digite o conteúdo da carta.'
     letterContainer.innerHTML = '';
     for (let word of words) {
       const newSpan = document.createElement('span');
       newSpan.innerText = word;
-      letterContainer.appendChild(newSpan)
+      letterContainer.appendChild(newSpan);
     }
   });
 }
