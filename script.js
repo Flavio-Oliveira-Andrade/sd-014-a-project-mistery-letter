65e8332f4e3d83c7f3ba44471b1f80f8f07ca25d
Paulo Renan Almeida <able.paulorenan@gmail.com>
2021-07-12 17:59:05 -0300

    100%, mesmo eu nao sabendo como o requisito 17 ta passando

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const classTamanho = ['medium', 'big', 'reallybig'];
 const classRotation = ['rotateleft', 'rotateright'];
 const classinclination = ['skewleft', 'skewright'];
 const contarPal = document.getElementById('carta-contador');
+const contText = document.getElementById('texto-contador');
 
 // https://pt.stackoverflow.com/questions/73214/criar-uma-combina%C3%A7%C3%A3o-aleatoria-com-classes-css-utilizando-javascript-ou-jquery
 function sorteia(arr) {
@@ -28,14 +29,14 @@ function gerarCarta() {
     for (let i = 0; i < arrayTexto.length; i += 1) {
       const palInp = document.createElement('span');
       palInp.innerText = arrayTexto[i];
-      //palInp.classList.add('palavras-separadas');
       palInp.classList.add(sorteia(classEstilo));
       palInp.classList.add(sorteia(classTamanho));
       palInp.classList.add(sorteia(classRotation));
       palInp.classList.add(sorteia(classinclination));
       carta.appendChild(palInp);
     }
-    contarPal.innerText = 'Palavras na carta: ' + arrayTexto.length;
+    contText.innerText = 'Palavras na carta: '
+    contarPal.innerText = arrayTexto.length;
   }
 }
 
