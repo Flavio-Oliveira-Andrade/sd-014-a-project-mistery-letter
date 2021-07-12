9569ea1f642525c2e62efc76564ab3dc99f0d757
Maurício Leite <mauricioleite1@gmail.com>
2021-07-12 19:14:56 -0300

    Requisito 03.1

--- script.js
+++ script.js
@@ -5,10 +5,12 @@ const pCarta = document.querySelector('#carta-gerada');
 btnCarta.addEventListener('click', () => {
   const texto = inputCarta.value;
   const textoDividido = texto.split(' ');
+
   textoDividido.forEach(gerarSpan);
+
   function gerarSpan(item) {
     const palavra = document.createElement('span');
     const spanGerado = pCarta.appendChild(palavra);
-    spanGerado.innerText = item + ' ';
+    spanGerado.innerText = item;
   }
 });
