eae6bc62612645e1bc46590910d5ff440cc7e853
Paulo Renan Almeida <able.paulorenan@gmail.com>
2021-07-12 17:04:37 -0300

    Requisito 16 que o npm nao aceita mas ta feito

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ const myInput = document.getElementById('carta-texto');
 const inputButton = document.getElementById('criar-carta');
 const carta = document.getElementById('carta-gerada');
 const cadaPal = document.getElementsByClassName('palavras-separadas');
+const classEstilo = ['newspaper', 'magazine1', 'magazine2', 'nada'];
+const classTamanho = ['medium', 'big', 'reallybig', 'nada'];
+const classRotation = ['rotateleft', 'rotateright', 'nada'];
+const classinclination = ['skewleft', 'skewright', 'nada'];
+
+// https://pt.stackoverflow.com/questions/73214/criar-uma-combina%C3%A7%C3%A3o-aleatoria-com-classes-css-utilizando-javascript-ou-jquery
+function sorteia(arr) {
+  var rand = Math.floor(Math.random() * arr.length);
+  return arr[rand];
+}
 
 function gerarCarta() {
   carta.innerText = '';
@@ -18,8 +28,15 @@ function gerarCarta() {
       const palInp = document.createElement('span');
       palInp.innerText = arrayTexto[i];
       palInp.classList.add('palavras-separadas');
+      palInp.classList.add(sorteia(classEstilo));
+      palInp.classList.add(sorteia(classTamanho));
+      palInp.classList.add(sorteia(classRotation));
+      palInp.classList.add(sorteia(classinclination));
       carta.appendChild(palInp);
     }
   }
 }
+
+
 inputButton.addEventListener('click', gerarCarta);
+//inputButton.addEventListener('click', addClasses);
