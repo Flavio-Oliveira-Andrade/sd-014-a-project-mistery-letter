5776c3a6e01a420781a448d1be86215786d772c0
Vincenzo Fedzuirek Di Giacomo <79121466+Vincenzofdg@users.noreply.github.com>
2021-07-12 19:14:05 -0300

    16-17° REQUISITO

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const textoDigitado = document.getElementById('carta-texto');
 const btnGerarCarta = document.getElementById('criar-carta');
 const corpoDaCarta = document.getElementById('carta-gerada');
-
+//----------------------------------------------------------//
+const style = ['newspaper', 'magazine1', 'magazine2'];
+const rotacao = ['rotateleft', 'rotateright'];
+const inclinacao = ['skewleft', 'skewright'];
+const tamanho = ['medium', 'big', 'reallybig'];
+//----------------------------------------------------------//
 
 // GERANDO A CARTA A PARTIR DO INPUT:
 btnGerarCarta.addEventListener('click', function () {
@@ -16,7 +21,56 @@ btnGerarCarta.addEventListener('click', function () {
             let gerandoPalavras = document.createElement('span');
             gerandoPalavras.id = 'span';
             gerandoPalavras.innerText = texto[index];
+            // SORTEANDO O STYLE
+            let sorteioStyle = Math.ceil(Math.random() * 3);
+            switch (true) {
+                case (sorteioStyle === 1):
+                    gerandoPalavras.classList.add('newspaper');
+                    break;
+                case (sorteioStyle === 2):
+                    gerandoPalavras.classList.add('magazine1');
+                    break;
+                case (sorteioStyle === 3):
+                    gerandoPalavras.classList.add('magazine2');
+                    break;
+            }
+            // SORTEANDO A ROTAÇÃO
+            let sorteioRota = Math.ceil(Math.random() * 2);
+            switch (true) {
+                case (sorteioRota === 1):
+                    gerandoPalavras.classList.add('rotateleft');
+                    break;
+                case (sorteioRota === 2):
+                    gerandoPalavras.classList.add('rotateright');
+                    break;
+            }
+            // SORTEANDO A INCLINAÇÃO
+            let sorteioAngulo = Math.ceil(Math.random() * 2);
+            switch (true) {
+                case (sorteioAngulo === 1):
+                    gerandoPalavras.classList.add('skewleft');
+                    break;
+                case (sorteioAngulo === 2):
+                    gerandoPalavras.classList.add('skewright');
+                    break;
+            }
+            // SORTEANDO O TAMANHO
+            let sorteioTamanho = Math.ceil(Math.random() * 3);
+            switch (true) {
+                case (sorteioTamanho === 1):
+                    gerandoPalavras.classList.add('medium');
+                    break;
+                case (sorteioTamanho === 2):
+                    gerandoPalavras.classList.add('big');
+                    break;
+                case (sorteioTamanho === 3):
+                    gerandoPalavras.classList.add('reallybig');
+                    break;
+            }
             corpoDaCarta.appendChild(gerandoPalavras)
         }
     }
+
+
 })
+
