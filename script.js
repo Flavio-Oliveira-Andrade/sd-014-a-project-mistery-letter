6c275ff2d27dcda5dba52bc4a2395b9e04e6c3af
Maurício Leite <mauricioleite1@gmail.com>
2021-07-12 19:38:28 -0300

    Requisito 03.2

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
+// Constantes e Variáveis
 const btnCarta = document.querySelector('#criar-carta');
 const inputCarta = document.querySelector('#carta-texto');
 const pCarta = document.querySelector('#carta-gerada');
-
+// Ao clicar no botão gera a carta
 btnCarta.addEventListener('click', () => {
-  const texto = inputCarta.value;
-  const textoDividido = texto.split(' ');
-
-  textoDividido.forEach(gerarSpan);
-
+  const texto = inputCarta.value.split(' ');
   function gerarSpan(item) {
     const palavra = document.createElement('span');
     const spanGerado = pCarta.appendChild(palavra);
     spanGerado.innerText = item;
   }
+  texto.forEach(gerarSpan);
 });
