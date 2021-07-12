f69c3d0a1ec020a49dc9bf898fc98c3fd2ef742d
Semnome1505 <Gabriel_1505@outlook.com>
2021-07-11 22:40:20 -0300

    Adiciona o Requisito 3.

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 const input = document.querySelector('#carta-texto');
 const button = document.querySelector('#criar-carta');
-const paragrafo =document.querySelector('#carta-gerada');
+const paragrafo = document.querySelector('#carta-gerada');
+
+function excluiSpans() {
+  const span = paragrafo.children;
+  if (span.length > 0) {
+    for (let i = span.length - 1; i >= 0; i -= 1) {
+      paragrafo.removeChild(span[i]);
+    }
+  }
+  separaPalavras();
+}
 
 function separaPalavras() {
+  if (input.value === '' || input.value == ' ') {
+    alert('Por favor, digite o conteúdo da carta.');
+  }
   const palavras = input.value.split(" ");
   for (let i of palavras) {
     const palavra = document.createElement('span');
@@ -11,4 +24,4 @@ function separaPalavras() {
   }
   console.log(paragrafo);
 }
-button.addEventListener('click', separaPalavras);
+button.addEventListener('click', excluiSpans);
