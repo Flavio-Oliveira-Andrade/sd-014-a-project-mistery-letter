eccc8e95003eff969910ed3283cc66193d2f7f31
Maurício Leite <mauricioleite1@gmail.com>
2021-07-12 19:08:38 -0300

    Requisitos 03

---
+++ script.js
@@ -0,0 +1,14 @@
+const btnCarta = document.querySelector('#criar-carta');
+const inputCarta = document.querySelector('#carta-texto');
+const pCarta = document.querySelector('#carta-gerada');
+
+btnCarta.addEventListener('click', () => {
+  const texto = inputCarta.value;
+  const textoDividido = texto.split(' ');
+  textoDividido.forEach(gerarSpan);
+  function gerarSpan(item) {
+    const palavra = document.createElement('span');
+    const spanGerado = pCarta.appendChild(palavra);
+    spanGerado.innerText = item + ' ';
+  }
+});
