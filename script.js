bd632ac5823da32bad2705cf11648e2940b932bb
Semnome1505 <Gabriel_1505@outlook.com>
2021-07-12 13:54:59 -0300

    Adiciona os Requisitos 16 e 17.

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 const input = document.querySelector('#carta-texto');
 const button = document.querySelector('#criar-carta');
 const paragrafo = document.querySelector('#carta-gerada');
+const estilo = ['newspaper', 'magazine1', 'magazine2'];
+const tamanho = ['medium', 'big', 'reallybig'];
+const rotacao = ['rotateleft', 'rotateright'];
+const inclinacao = ['skewleft', 'skewright'];
+const grupos = [estilo, tamanho, rotacao, inclinacao];
+
+function classes() {
+  const grupo1 = Math.floor(Math.random() * 4);
+  let grupo2 = Math.floor(Math.random() * 4);
+  if (grupo1 === grupo2) {
+    if (grupo2 === 0) {
+      grupo2 += 1;
+    } else if (grupo2 <= 3) {
+      grupo2 -= 1;
+    }
+  }
+  const index1 = Math.floor(Math.random() * grupos[grupo1].length);
+  const index2 = Math.floor(Math.random() * grupos[grupo2].length);
+  let classe1 = grupos[grupo1];
+  classe1 = classe1[index1];
+  let classe2 = grupos[grupo2];
+  classe2 = classe2[index2];
+  return classe1, classe2;
+  // console.log(classe1);
+  // console.log(classe2);
+}
 
 function separaPalavras() {
   if (input.value === '' || input.value === ' ') {
@@ -11,6 +37,8 @@ function separaPalavras() {
     for (let i = 0; i < palavras.length; i += 1) {
       const palavra = document.createElement('span');
       palavra.innerText = palavras[i];
+      palavra.classList.add(classes());
+      palavra.classList.add(classes());
       paragrafo.appendChild(palavra);
     }
   }
