05448afc521efdb6bb0f4269e61b3301f89ab478
unamednada <dias.gustavo.dias@gmail.com>
2021-07-12 15:06:10 -0300

    Fix style lint and add changeclass

--- script.js
+++ script.js
@@ -25,25 +25,39 @@ const sizeGroup = ['medium', 'big', 'reallybig'];
 const rotationGroup = ['rotateleft', 'rotateright'];
 const inclinationGroup = ['skewleft', 'skewright'];
 
+function addClasstoWord(currentWord) {
+  const groupList = [styleGroup, sizeGroup, rotationGroup, inclinationGroup];
+  const random4j = Math.floor(Math.random() * 3) + 2;
+  for (let j = 0; j < random4j; j += 1) {
+    const random4 = Math.floor(Math.random() * (4 - j));
+    const currentGroup = groupList[random4];
+    const random3 = Math.floor(Math.random() * 3);
+    const random2 = Math.floor(Math.random() * 2);
+    if ([styleGroup, sizeGroup].includes(currentGroup)) {
+      currentWord.classList.add(currentGroup[random3]);
+    } else {
+      currentWord.classList.add(currentGroup[random2]);
+    }
+    groupList.splice(groupList.indexOf(currentGroup), 1);
+  }
+}
+
 function addClass() {
   const wordArray = Array.from(paragraph.children);
   for (let i = 0; i < wordArray.length; i += 1) {
-    const groupList = [styleGroup, sizeGroup, rotationGroup, inclinationGroup];
     const currentWord = wordArray[i];
-    const random4j = Math.floor(Math.random() * 3) + 2;
-    for (let j = 0; j < random4j; j += 1) {
-      const random4 = Math.floor(Math.random() * (4 - j));
-      const currentGroup = groupList[random4];
-      const random3 = Math.floor(Math.random() * 3);
-      const random2 = Math.floor(Math.random() * 2);
-      if ([styleGroup, sizeGroup].includes(currentGroup)) {
-        currentWord.classList.add(currentGroup[random3]);
-      } else {
-        currentWord.classList.add(currentGroup[random2]);
-      }
-      groupList.splice(groupList.indexOf(currentGroup), 1);
-    }
+    addClasstoWord(currentWord);
   }
 }
 
 generateLetterBtn.addEventListener('click', addClass);
+
+function changeClass(event) {
+  if (event.target.className !== 'carta-gerada') {
+    const currentWord = event.target;
+    currentWord.classList = [];
+    addClasstoWord(currentWord);
+  }
+}
+
+paragraph.addEventListener('click', changeClass);
