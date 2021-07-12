45bde3535ec381682fe8f8945dee8038c620a057
unamednada <dias.gustavo.dias@gmail.com>
2021-07-12 11:44:18 -0300

    Create alertEmpty letter with regex

--- script.js
+++ script.js
@@ -2,7 +2,18 @@ const generateLetterBtn = document.querySelector('#criar-carta');
 const paragraph = document.querySelector('#carta-gerada');
 const input = document.querySelector('#carta-texto');
 
+// Inspiração para Regex: https://stackoverflow.com/questions/10398931/how-to-remove-text-from-a-string
+
+function alertEmptyLetter() {
+  const message = input.value.replace(/ /g, '');
+  console.log(message);
+  if (!input.value || !message) {
+    window.alert('Por favor, digite o conteúdo da carta.');
+  }
+}
+
 function generateLetter() {
+  alertEmptyLetter();
   paragraph.innerHTML = '';
   const wordArray = Array.from(input.value.split(' '));
   for (let i = 0; i < wordArray.length; i += 1) {
