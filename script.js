744f61b03ae04f0fbcf94effd8d9933b188dec8e
MuriloM <muriloommaia@gmail.com>
2021-07-12 17:07:37 -0300

    requisito 5

--- script.js
+++ script.js
@@ -2,8 +2,14 @@ const button = document.querySelector('#criar-carta');
 const paragrafo = document.querySelector('#carta-gerada');
 
 function geraCarta () {
-    const input = document.querySelector('#carta-texto').value;
-    let arrayInput = input.split(' ');
+    const input = document.querySelector('#carta-texto');
+    const regex = /\w/g
+    if (!regex.test(input.value)) {
+        paragrafo.innerText = "Por favor, digite o conteúdo da carta.";
+        input.value = "";
+        return 
+    }
+    let arrayInput = input.value.split(' ');
     console.log(arrayInput)
     for (let i of arrayInput) {
         const span = document.createElement('span');
