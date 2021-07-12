10deaa1b9be3d6427a8b65eb69468fb4637324cf
Paulo Renan Almeida <able.paulorenan@gmail.com>
2021-07-12 17:52:43 -0300

    Requisito 16 certo agora

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const myInput = document.getElementById('carta-texto');
 const inputButton = document.getElementById('criar-carta');
 const carta = document.getElementById('carta-gerada');
-const cadaPal = document.getElementsByClassName('palavras-separadas');
-const classEstilo = ['newspaper', 'magazine1', 'magazine2', 'nada'];
-const classTamanho = ['medium', 'big', 'reallybig', 'nada'];
-const classRotation = ['rotateleft', 'rotateright', 'nada'];
-const classinclination = ['skewleft', 'skewright', 'nada'];
+const cadaPal = document.querySelectorAll('#carta-gerada span');
+const classEstilo = ['newspaper', 'magazine1', 'magazine2'];
+const classTamanho = ['medium', 'big', 'reallybig'];
+const classRotation = ['rotateleft', 'rotateright'];
+const classinclination = ['skewleft', 'skewright'];
+const contarPal = document.getElementById('carta-contador');
 
 // https://pt.stackoverflow.com/questions/73214/criar-uma-combina%C3%A7%C3%A3o-aleatoria-com-classes-css-utilizando-javascript-ou-jquery
 function sorteia(arr) {
@@ -18,7 +19,7 @@ function gerarCarta() {
   if (myInput.value.trim() === '') {
     carta.innerText = 'Por favor, digite o conteúdo da carta.';
   } else {
-    const arrayTexto = myInput.value.split(' ');
+    const arrayTexto = myInput.value.trim().split(' ');
     for (let i = 0; i < cadaPal.length; i += 1) {
       const element = cadaPal[i];
       carta.removeChild(element);
@@ -27,13 +28,14 @@ function gerarCarta() {
     for (let i = 0; i < arrayTexto.length; i += 1) {
       const palInp = document.createElement('span');
       palInp.innerText = arrayTexto[i];
-      palInp.classList.add('palavras-separadas');
+      //palInp.classList.add('palavras-separadas');
       palInp.classList.add(sorteia(classEstilo));
       palInp.classList.add(sorteia(classTamanho));
       palInp.classList.add(sorteia(classRotation));
       palInp.classList.add(sorteia(classinclination));
       carta.appendChild(palInp);
     }
+    contarPal.innerText = 'Palavras na carta: ' + arrayTexto.length;
   }
 }
 
