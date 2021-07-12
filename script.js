f29721c05c44e21580358e8e92433539d7959e98
darthyd <luaks.skt8@gmail.com>
2021-07-11 22:45:13 -0300

    REQUISITOS 7-18

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 const input = document.querySelector('#carta-texto');
 const createLetter = document.querySelector('#criar-carta');
 const letterContent = document.querySelector('#carta-gerada');
+const countWords = document.querySelector('#carta-contador');
+const classes = [
+  'newspaper',
+  'magazine1',
+  'magazine2',
+  'medium',
+  'big',
+  'reallybig',
+  'rotateleft',
+  'rotateright',
+  'skewleft',
+  'skewright',
+];
 
 function changeLetter() {
   if (input.value.trim() === '') return 'Por favor, digite o conteúdo da carta.';
   const arr = input.value.split(' ');
   const spans = arr.map((e) => (
-    `<span>${e}</span>`
+    `<span class="${classes[Math.round(Math.random() * (classes.length - 1))]}">${e}</span>`
   ));
+  countWords.innerText = input.value.split(' ').length;
   return spans.join('');
 }
 
