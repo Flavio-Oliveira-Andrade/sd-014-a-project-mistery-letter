ca1b65e76a03838b2791fb1aeab7982cb1697008
unamednada <dias.gustavo.dias@gmail.com>
2021-07-12 11:22:38 -0300

    Create generateLetter function

---
+++ script.js
@@ -0,0 +1,14 @@
+const generateLetterBtn = document.querySelector('#criar-carta');
+const paragraph = document.querySelector('#carta-gerada');
+const input = document.querySelector('#carta-texto');
+
+function generateLetter() {
+  const wordArray = Array.from(input.value.split(' '));
+  for (let i = 0; i < wordArray.length; i += 1) {
+    const currentWord = document.createElement('span');
+    currentWord.innerText(wordArray[i]);
+    paragraph.appendChild(currentWord);
+  }
+}
+
+generateLetterBtn.addEventListener('click', generateLetter);
