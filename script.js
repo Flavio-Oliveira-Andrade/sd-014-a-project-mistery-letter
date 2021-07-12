21a1dbc95c109a9f0f5d01419ed6b15c603c4fc0
WilsonSalles-Jnr <wilsonsallesjunior@gmail.com>
2021-07-12 20:06:35 -0300

    faltando refatorar

--- script.js
+++ script.js
@@ -3,73 +3,72 @@ const par = document.getElementById('carta-gerada');
 const btn = document.getElementById('criar-carta');
 const contador = document.getElementById('carta-contador');
 
-let wCount = 0;
-
 contador.innerText = document.getElementsByTagName('span').length;
 
 const estilo = ['newspaper', 'magazine1', 'magazine2', 'rotateleft', 'rotateright'];
 const sizegp = ['medium', 'big', 'reallybig', 'skewleft', 'skewright'];
-// const rotgp = ['rotateleft', 'rotateright'];
-// const skewgp = ['skewleft', 'skewright'];
-
-function checkSpaces() {
-  const palavra = input.value;
-  if (palavra[0] === ' ') {
-    for (const index in palavra) {
-      let contaEspaco = 0;
-      if (palavra[index] === ' ') {
-        contaEspaco += 1;
-      }
-      if (palavra.length === contaEspaco) {
-        return 'close';
-      }
-    }
-  }
-}
 
 function checkNull() {
   if (input.value === '') {
+    par.innerText = 'Por favor, digite o conteúdo da carta.';
     return 'close';
   }
 }
 
-function gerar() {
+function checkSpaces() {
   const palavra = input.value;
+  let textual = '';
+
+  for (let t = 0; t < palavra.length; t += 1) {
+    textual += ' ';
+  }
+
+  if (textual === palavra) {
+    par.innerText = 'Por favor, digite o conteúdo da carta.';
+    return 'close';
+  }
+  checkNull();
+}
+
+function wcount() {
+  contador.innerText = document.getElementsByTagName('span').length;
+}
+function clear() {
   while (par.firstChild) {
     par.removeChild(par.firstChild);
   }
+}
+function String(texting) {
+  const span = document.createElement('span');
+  span.innerText = texting;
+  span.className += `${estilo[Math.floor(Math.random() * 5)]} `;
+  span.className += sizegp[Math.floor(Math.random() * 5)];
+  par.appendChild(span);
+}
+function lastString(texting) {
+  const lastSpan = document.createElement('span');
+  lastSpan.className += `${estilo[Math.floor(Math.random() * 5)]} `;
+  lastSpan.className += sizegp[Math.floor(Math.random() * 5)];
+  lastSpan.innerText = texting;
+  par.appendChild(lastSpan);
+}
 
-  wCount = 0;
-  if (checkSpaces() === 'close' || checkNull === 'close') {
-    par.innerText = 'Por favor, digite o conteúdo da carta.';
-  } else {
+function gerar() {
+  const palavra = input.value;
+  clear();
+  if (checkSpaces() !== 'close') {
     let string = '';
-    for (const i in palavra) {
+    for (let i = 0; i < palavra.length; i += 1) {
       if (input.value[i] !== ' ') {
         string += input.value[i];
       } else {
-        // string += ' '
-        const span = document.createElement('span');
-        span.innerText = string;
-        span.className += `${estilo[Math.floor(Math.random() * 5)]} `;
-        span.className += sizegp[Math.floor(Math.random() * 5)];
-        // span.className += rotgp[Math.floor(Math.random()*2)] + " "
-        // span.className += skewgp[Math.floor(Math.random()*2)]
+        String(string);
         string = '';
-        par.appendChild(span);
-        wCount += 1;
-        // par.innerText += ' '
       }
     }
-    const lastSpan = document.createElement('span');
-    lastSpan.className += `${estilo[Math.floor(Math.random() * 5)]} `;
-    lastSpan.className += sizegp[Math.floor(Math.random() * 5)];
-    // lastSpan.className += rotgp[Math.floor(Math.random()*2)] + " "
-    // lastSpan.className += skewgp[Math.floor(Math.random()*2)]
-    lastSpan.innerText = string;
-    wCount += 1;
-    par.appendChild(lastSpan);
+    lastString(string);
   }
-  contador.innerText = wCount;
+  wcount();
 }
+
 btn.addEventListener('click', gerar);
