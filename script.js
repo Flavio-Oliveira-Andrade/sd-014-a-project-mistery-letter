ccaa22b10212122f7fe25f422288fccc1f183b96
darthyd <luaks.skt8@gmail.com>
2021-07-12 01:29:53 -0300

    give some style

--- script.js
+++ script.js
@@ -1,25 +1,47 @@
 const input = document.querySelector('#carta-texto');
+const title = document.querySelector('#title');
 const createLetter = document.querySelector('#criar-carta');
 const letterContent = document.querySelector('#carta-gerada');
 const countWords = document.querySelector('#carta-contador');
-const classes = [
-  'newspaper',
-  'magazine1',
-  'magazine2',
-  'medium',
-  'big',
-  'reallybig',
-  'rotateleft',
-  'rotateright',
-  'skewleft',
-  'skewright',
-];
+
+const classes = {
+  background: [
+    'newspaper',
+    'magazine1',
+    'magazine2',
+  ],
+  size: [
+    'medium',
+    'big',
+    'reallybig',
+  ],
+  rotation: [
+    'rotateleft',
+    'rotateright',
+  ],
+  skew: [
+    'skewleft',
+    'skewright',
+  ],
+};
+
+function generateStyle() {
+  return (
+    `${classes.background[Math.round(Math.random() * (classes.background.length - 1))]
+    } ${classes.size[Math.round(Math.random() * (classes.size.length - 1))]
+    } ${classes.rotation[Math.round(Math.random())]
+    } ${classes.skew[Math.round(Math.random())]}`
+  );
+}
+
+title.innerHTML = `<span class="${generateStyle()
+}">Mystery</span><span class="${generateStyle()}">Letter</span>`;
 
 function changeLetter() {
   if (input.value.trim() === '') return 'Por favor, digite o conteúdo da carta.';
   const arr = input.value.split(' ');
   const spans = arr.map((e) => (
-    `<span class="${classes[Math.round(Math.random() * (classes.length - 1))]}">${e}</span>`
+    `<span class="${generateStyle()}">${e}</span>`
   ));
   countWords.innerText = input.value.split(' ').length;
   return spans.join('');
@@ -28,3 +50,5 @@ function changeLetter() {
 createLetter.addEventListener('click', () => {
   letterContent.innerHTML = changeLetter();
 });
+
+console.log(generateStyle());
