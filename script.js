213db85979607fe71fc5f61ccff29da961d3235f
unamednada <dias.gustavo.dias@gmail.com>
2021-07-12 11:34:05 -0300

    Fix paragraph initial innerHTML

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ const paragraph = document.querySelector('#carta-gerada');
 const input = document.querySelector('#carta-texto');
 
 function generateLetter() {
+  paragraph.innerHTML = '';
   const wordArray = Array.from(input.value.split(' '));
   for (let i = 0; i < wordArray.length; i += 1) {
     const currentWord = document.createElement('span');
     currentWord.innerText = wordArray[i];
     paragraph.appendChild(currentWord);
   }
+  console.log(wordArray);
 }
 
 generateLetterBtn.addEventListener('click', generateLetter);
