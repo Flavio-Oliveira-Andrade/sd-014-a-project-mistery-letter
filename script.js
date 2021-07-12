46d025413bc42d358cac301a306950dfc00a1a7f
Vincenzo Fedzuirek Di Giacomo <79121466+Vincenzofdg@users.noreply.github.com>
2021-07-12 17:47:42 -0300

    3° REQUISITO

---
+++ script.js
@@ -0,0 +1,17 @@
+const textoDigitado = document.getElementById('carta-texto');
+const btnGerarCarta = document.getElementById('criar-carta');
+const corpoDaCarta = document.getElementById('carta-gerada');
+
+// GERANDO A CARTA A PARTIR DO INPUT:
+btnGerarCarta.addEventListener('click', function() {
+    while(corpoDaCarta.firstChild) {
+        corpoDaCarta.removeChild(corpoDaCarta.firstChild)
+    }
+    let texto = textoDigitado.value.split(' ');
+    for (let index = 0; index < texto.length; index++) {
+        let gerandoPalavras = document.createElement('span');
+        gerandoPalavras.id = 'span';
+        gerandoPalavras.innerText = texto[index];
+        corpoDaCarta.appendChild(gerandoPalavras)
+    }
+})
