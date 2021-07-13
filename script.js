ba84f6ce4d45fd46737959c93234e3b9fed9ebd8
Pablo Assunção <pabloassuncao2@gmail.com>
2021-07-13 20:13:47 -0300

    C3 - Adiciona o requiito 16 o 18

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
 const btnCreate = document.querySelector('#criar-carta');
 const letterResult = document.querySelector('#carta-gerada');
 const letterInput = document.querySelector('#carta-texto');
+const classes = {
+  0: {
+    0: 'newspaper',
+    1: 'magazine1',
+    2: 'magazine2',
+  },
+  1: {
+    0: 'medium',
+    1: 'big',
+    2: 'reallybig',
+  },
+  2: {
+    0: 'rotateleft',
+    1: 'rotateright',
+  },
+  3: {
+    0: 'skewleft',
+    1: 'skewright',
+  },
+};
+const wordCounter = document.querySelector('#carta-contador');
 
 const allF = {
   create: function newLetter(string, result) {
-    allF.clearLetter();
-    allF.checker(letterInput.value);
     const words = string.split(' ');
     for (let index = 0; index < words.length; index += 1) {
       const wordSpace = document.createElement('span');
@@ -21,10 +40,28 @@ const allF = {
   clearLetter: function clear() {
     letterResult.innerHTML = '';
   },
+  randomizer: function randomStyles(param, class1) {
+    const toStyle = document.querySelectorAll(param);
+    for (let index = 0; index < toStyle.length; index += 1) {
+      const a1 = Math.floor(Math.random() * 2);
+      const a2 = Math.floor(Math.random() * 3);
+      const b1 = Math.floor(Math.random() * 2);
+      const b2 = Math.floor(Math.random() * 2);
+      toStyle[index].classList.add(class1[a1][a2]);
+      toStyle[index].classList.add(class1[b1][b2]);
+    }
+  },
+  wordCount: function words(params) {
+    wordCounter.innerText = document.querySelectorAll(params).length;
+  },
 };
 
 // Event Listener
 // Ao clicar criar a carta
 btnCreate.addEventListener('click', () => {
+  allF.clearLetter();
+  allF.checker(letterInput.value);
   allF.create(letterInput.value, letterResult);
+  allF.randomizer('span', classes);
+  allF.wordCount('span');
 });
