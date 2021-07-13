47c29f3e81ce74cd8422e3d702b47f6d7873558a
Matheus Souza <ms.conrado25@hotmail.com>
2021-07-12 23:16:51 -0300

    arrumando algumas cosias do lint

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 const paragrafoCarta = document.getElementById('carta-gerada');
 const btnCriarCarta = document.getElementById('criar-carta');
 const inputCarta = document.getElementById('carta-texto');
+// const gpEstilo = ['newspaper', 'magazine1', 'magazine2'];
+// const gpTamanho = ['medium', 'big', 'reallybig'];
+// const gpRotacao = ['rotateleft', 'rotateright'];
+// const gpInclinacao = ['skewleft', 'skewright'];
 
+const classEstilos = ['newspaper', 'magazine1', 'magazine2','medium', 'big', 'reallybig','rotateleft', 'rotateright','skewleft', 'skewright'];
+
+console.log(classEstilos)
 btnCriarCarta.addEventListener('click', function () {
-  if (inputCarta.value !== String) {
+  if (inputCarta.value === '' ) {
     paragrafoCarta.innerText = 'Por favor, digite o conteúdo da carta.'
   } else {
     const separador = inputCarta.value.split(' ')
-    console.log(separador);
     for (const inserir of separador) {
       const spanTexto = document.createElement('span');
-      spanTexto.innerText = inserir + ' ';
+      spanTexto.innerText = inserir;
+      const numRandom = Math.floor(Math.random() * 10);
+      spanTexto.classList.add(classEstilos[numRandom]);
+      spanTexto.classList.add(classEstilos[numRandom]);
+      spanTexto.classList.add(classEstilos[numRandom]);
+      spanTexto.classList.add(classEstilos[numRandom]);
       paragrafoCarta.appendChild(spanTexto);
     }
   }
