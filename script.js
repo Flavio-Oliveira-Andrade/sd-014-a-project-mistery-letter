8f448f104cb6c769f7a5ad52081f1d867079a619
Maurício Leite <mauricioleite1@gmail.com>
2021-07-12 21:09:24 -0300

    Commit BOMBA :D classes criadas

--- script.js
+++ script.js
@@ -6,19 +6,27 @@ const errorMessage = 'Por favor, digite o conteúdo da carta.';
 function criarCarta() {
   carta.innerText = '';
   const texto = input.value.split(' ');
-  texto.forEach(gerarCarta);
   function gerarCarta(item) {
     const palavra = document.createElement('span');
     carta.appendChild(palavra).innerText = item;
   }
-};
+  texto.forEach(gerarCarta);
+}
 // Botão executa função de criar carta
-btn.addEventListener('click', () => { 
-  input.value && input.value.trim() ? criarCarta() : carta.innerText = errorMessage;
+btn.addEventListener('click', () => {
+  if (input.value && input.value.trim()) {
+    criarCarta();
+  } else {
+    carta.innerText = errorMessage; 
+  }
 });
 // Enter executa função de criar carta
 input.addEventListener('keyup', (event) => {
   if (event.key === 'Enter') { 
-    input.value && input.value.trim() ? criarCarta() : carta.innerText = errorMessage;
-   } 
+    if (input.value && input.value.trim()) {
+      criarCarta();
+    } else {
+      carta.innerText = errorMessage; 
+    }
+  }
 });
