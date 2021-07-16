238df7ed6b46b89ce7000c928d1dc95d51475c2f
Michel <ferrazmichel@gmail.com>
2021-07-16 15:20:45 -0300

    Ajustes finais 2

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const classes2 = ['medium', 'big', 'reallybig'];
 const classes3 = ['rotateleft', 'rotateright'];
 const classes4 = ['skewleft', 'skewright'];
 const paragrafo = document.getElementById('carta-gerada');
+const nome1 = 'carta-contador';
+const nome2 = 'carta-texto';
 
 function limparParagrafo() {
   const limpar = document.getElementsByTagName('span');
@@ -10,34 +12,31 @@ function limparParagrafo() {
 }
 
 function addContador() {
-
-  if (document.getElementById('carta-contador') === null) {
-    const input = document.getElementById('carta-texto').value;
+  if (document.getElementById(nome1) === null) {
+    const input = document.getElementById(nome2).value;
     const array = input.split(' ');
     const contador = document.createElement('p');
-    contador.setAttribute('id', 'carta-contador');
+    contador.setAttribute('id', nome1);
     contador.innerText = array.length;
     document.body.appendChild(contador);
   } else {
-    const removeCont = document.getElementById('carta-contador');
+    const removeCont = document.getElementById(nome1);
     removeCont.parentNode.removeChild(removeCont);
-    const input = document.getElementById('carta-texto').value;
+    const input = document.getElementById(nome2).value;
     const array = input.split(' ');
     const contador = document.createElement('p');
-    contador.setAttribute('id', 'carta-contador');
+    contador.setAttribute('id', nome1);
     contador.innerText = array.length;
     document.body.appendChild(contador);
   }
 }
 
-// eslint-disable-next-line max-lines-per-function
 function gerarCarta() {
-  const input = document.getElementById('carta-texto').value;
+  const input = document.getElementById(nome2).value;
   const array = input.split(' ');
   for (let i = 0; i < array.length; i += 1) {
-    if (array[i] === '') { } else {
-      const span = document.createElement('span');
-      span.innerText = array[i];
+    if (array[i] !== '') {
+      const span = document.createElement('span'); span.innerText = array[i];
       const x1 = Math.floor(Math.random() * 3);
       const nomeClasse1 = classes1[x1];
       const x2 = Math.floor(Math.random() * 3);
@@ -55,7 +54,7 @@ function gerarCarta() {
 
 function paragrafoVazio() {
   limparParagrafo.call();
-  const input = document.getElementById('carta-texto').value;
+  const input = document.getElementById(nome2).value;
   if (input === null || input.match(/^ *$/) !== null) {
     const span = document.createElement('span');
     span.innerText = 'Por favor, digite o conteúdo da carta.';
