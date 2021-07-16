1e73c8aa26d1f9d1f189ce3c8f2f5f6dbb0dce7f
Douglas Souza <douglassouzabjj@hotmail.com>
2021-07-15 21:55:20 -0300

    Corrige requisito #3 e adiciona requisito #5

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
-const msg = document.getElementById('carta-texto')
+const msg = document.getElementById('carta-texto');
 const btnCriar = document.getElementById('criar-carta');
 const carta = document.getElementById('carta-gerada');
 
 function createLetter() {
   const words = msg.value.split(' ');
-  for (let index = 0; index < words.length; index += 1) {
-    const word = document.createElement('span');
-    word.innerText = words[index];
-    carta.appendChild(word);
+
+  if (msg.value.trim().length === 0) {
+    carta.innerText = 'Por favor, digite o conteúdo da carta.';
+  } else {
+    carta.innerText = '';
+    for (let index = 0; index < words.length; index += 1) {
+      const word = document.createElement('span');
+      word.innerText = words[index];
+      carta.appendChild(word);
+    }
   }
+
 }
 btnCriar.addEventListener('click', createLetter);
