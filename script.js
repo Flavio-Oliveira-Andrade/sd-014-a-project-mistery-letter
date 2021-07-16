99253d6fcadaaeef763a843f91d8d1e53791eb40
Douglas Souza <douglassouzabjj@hotmail.com>
2021-07-15 22:26:18 -0300

    Adiciona requisitos #16 e #17

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const msg = document.getElementById('carta-texto');
 const btnCriar = document.getElementById('criar-carta');
 const carta = document.getElementById('carta-gerada');
+const estilo = ['newspaper', 'magazine1', 'magazine2'];
+const tamanho = ['medium', 'big', 'reallybig'];
+const rotacao = ['rotateleft', 'rotateright'];
+const inclinacao = ['skewleft', 'skewright'];
 
 function createLetter() {
   const words = msg.value.split(' ');
@@ -12,9 +16,13 @@ function createLetter() {
     for (let index = 0; index < words.length; index += 1) {
       const word = document.createElement('span');
       word.innerText = words[index];
+      word.classList.add(estilo[Math.floor(Math.random() * estilo.length)]);
+      word.classList.add(tamanho[Math.floor(Math.random() * tamanho.length)]);
+      word.classList.add(rotacao[Math.floor(Math.random() * rotacao.length)]);
+      word.classList.add(inclinacao[Math.floor(Math.random() * inclinacao.length)]);
       carta.appendChild(word);
     }
   }
-
 }
+
 btnCriar.addEventListener('click', createLetter);
